7a23d6cfa6b702a1acfdd7674ec44d7c06b2b997
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-11-12 10:40:41 +0300

    Updated the models database structure

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy
+db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = "users"
@@ -9,6 +10,7 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(20), default="customer")
+
 
 class Product(db.Model):
     __tablename__ = "products"
@@ -19,12 +21,14 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(50), nullable=False)
 
+
 class Cart(db.Model):
     __tablename__ = "carts"
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     items = db.relationship('CartItem', backref='cart', lazy=True)
+
 
 class CartItem(db.Model):
     __tablename__ = "cart_items"
