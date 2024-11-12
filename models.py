a9ccebabaa479df0ee4e72ae897cf052770d5a7e
Margaret Mwangi <margaret.mwangi3@moringaschool.com>
2024-11-12 11:44:06 +0300

    updated seed.py

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
 db = SQLAlchemy()
 
 
-class Customer (db.Model):
+class Customer(db.Model):
     __tablename__ = 'customers'
+    
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
@@ -15,36 +15,48 @@ class Customer (db.Model):
     address = db.Column(db.String(120), nullable=False)
     phone_number = db.Column(db.String(15))
     
-    
+    orders = db.relationship('Order', back_populates='customer', lazy=True)
+
+
 class Company(db.Model):
     __tablename__ = 'companies'
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
-    products = db.relationship('Product', backref='company', lazy=True)
-    
-    
+    products = db.relationship('Products', backref='company', lazy=True)
+
+
 class Category(db.Model):
     __tablename__ = 'categories'
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
-    products = db.relationship('Product', backref='category', lazy=True)
     
-    
+    products = db.relationship('Products', backref='category', lazy=True)
+
+
 class Products(db.Model):
     __tablename__ = 'products'
+    
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(100), nullable=False)
     product_model = db.Column(db.String(50))
     price = db.Column(db.Float, nullable=False)
+    
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
     company_id = db.Column(db.Integer, db.ForeignKey('companies.id'), nullable=False)
+    
     photo_url = db.Column(db.String(200))
-    
-    
+
+
 class Order(db.Model):
     __tablename__ = 'orders'
+    
     id = db.Column(db.Integer, primary_key=True)
+    
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    
     order_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     total = db.Column(db.Float, nullable=False)
-    customer = db.relationship('Customer',backref='orders', lazy=True)
+    
+    customer = db.relationship('Customer', back_populates='orders', lazy=True)
